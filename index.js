2552440129c621a848a80f83e7ecb4f2b0d7f472
slindgre <slindgre@nebrwesleyan.edu>
2014-12-04 09:36:36 -0600

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function key_pressed_down(event) {
         }
     }
     if (event.keyCode === down_arrow_key) {
-        if (is_in_bounds(protagonist.x, protagonist.y + 1)&&((get_terrain_height(protagonist.x, protagonist.y + 1)-get_terrain_height(protagonist.x, protagonist.y)) < 2)&&(get_terrain_cell(protagonist.x, protagonist.y + 1)[1] != [treeshort]) ) {
+        if (is_in_bounds(protagonist.x, protagonist.y + 1)&&((get_terrain_height(protagonist.x, protagonist.y + 1)-get_terrain_height(protagonist.x, protagonist.y)) < 2)&&(no_tree(x, y) != treeshort) ) {
             protagonist.y = protagonist.y + 1;
         }
     }

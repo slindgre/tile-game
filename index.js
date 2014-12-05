768537a087ac5c80cf95167228d0621b3f89a441
slindgre <slindgre@nebrwesleyan.edu>
2014-12-05 11:17:10 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,31 +7,30 @@ var left_arrow_key = 37;
 var up_arrow_key = 38;
 var right_arrow_key = 39;
 var down_arrow_key = 40;
-
 function key_pressed_down(event) {
-    occupants[protagonist.y][protagonist.x] = undefined;
-    if (event.keyCode === left_arrow_key) {
-        if (is_in_bounds(protagonist.x - 1, protagonist.y)&&((get_terrain_height(protagonist.x - 1, protagonist.y)-get_terrain_height(protagonist.x, protagonist.y)) < 2)&&(no_tree(protagonist.x - 1, protagonist.y, 1)) ) {
-            protagonist.x = protagonist.x - 1;
-        }
-    }
-    if (event.keyCode === right_arrow_key) {
-        if (is_in_bounds(protagonist.x + 1, protagonist.y)&&((get_terrain_height(protagonist.x + 1, protagonist.y)-get_terrain_height(protagonist.x, protagonist.y)) < 2)&&(no_tree(protagonist.x + 1, protagonist.y, 1) )) {
-            protagonist.x = protagonist.x + 1;
-        }
-    }
-    if (event.keyCode === up_arrow_key) {
-        if (is_in_bounds(protagonist.x, protagonist.y - 1)&&((get_terrain_height(protagonist.x, protagonist.y - 1)-get_terrain_height(protagonist.x, protagonist.y)) < 2)&&(no_tree(protagonist.x, protagonist.y - 1, 1)) ) {
-            protagonist.y = protagonist.y - 1;
-        }
-    }
-    if (event.keyCode === down_arrow_key) {
-        if (is_in_bounds(protagonist.x, protagonist.y + 1)&&((get_terrain_height(protagonist.x, protagonist.y + 1)-get_terrain_height(protagonist.x, protagonist.y)) < 2)&&(no_tree(protagonist.x, protagonist.y + 1, 1) ) ) {
-            protagonist.y = protagonist.y + 1;
-        }
-    }
-    occupants[protagonist.y][protagonist.x] = protagonist.element;
-    render();
+occupants[protagonist.y][protagonist.x] = undefined;
+if (event.keyCode === left_arrow_key) {
+if (is_in_bounds(protagonist.x - 1, protagonist.y)&&((get_terrain_height(protagonist.x - 1, protagonist.y)-get_terrain_height(protagonist.x, protagonist.y)) < 2)&&(no_tree(protagonist.x - 1, protagonist.y, 1)) ) {
+protagonist.x = protagonist.x - 1;
+}
+}
+if (event.keyCode === right_arrow_key) {
+if (is_in_bounds(protagonist.x + 1, protagonist.y)&&((get_terrain_height(protagonist.x + 1, protagonist.y)-get_terrain_height(protagonist.x, protagonist.y)) < 2)&&(no_tree(protagonist.x + 1, protagonist.y, 1) )) {
+protagonist.x = protagonist.x + 1;
+}
+}
+if (event.keyCode === up_arrow_key) {
+if (is_in_bounds(protagonist.x, protagonist.y - 1)&&((get_terrain_height(protagonist.x, protagonist.y - 1)-get_terrain_height(protagonist.x, protagonist.y)) < 2)&&(no_tree(protagonist.x, protagonist.y - 1, 1)) ) {
+protagonist.y = protagonist.y - 1;
+}
+}
+if (event.keyCode === down_arrow_key) {
+if (is_in_bounds(protagonist.x, protagonist.y + 1)&&((get_terrain_height(protagonist.x, protagonist.y + 1)-get_terrain_height(protagonist.x, protagonist.y)) < 2)&&(no_tree(protagonist.x, protagonist.y + 1, 1) ) ) {
+protagonist.y = protagonist.y + 1;
+}
+}
+occupants[protagonist.y][protagonist.x] = protagonist.element;
+render();
 }
 
 function score(event) {

c4cd4a1a53f09620edf1d48b7ff1d2e8cc09f5ac
slindgre <slindgre@nebrwesleyan.edu>
2014-12-07 20:45:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -80,5 +80,5 @@ render();
 }
 
 
- document.getElementById('score').innerHTML= score(event);
+ document.getElementById('score').innerHTML= score();
 document.addEventListener('keydown', key_pressed_down);

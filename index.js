0e698815785072f389d4334d95a31d5acf3648c5
slindgre <slindgre@nebrwesleyan.edu>
2014-12-07 20:54:49 -0600

    Update index.js

--- index.js
+++ index.js
@@ -24,32 +24,32 @@ function score(event) {
 if (event.keyCode === left_arrow_key) {
 if (get_occupants_cell(protagonist.x - 1, protagonist.y) !==  undefined) {
 add();
-
+ return i;
 }
-
+ return i;
 }
 if (event.keyCode === right_arrow_key) {
 if (get_occupants_cell(protagonist.x + 1, protagonist.y) !==  undefined) {
 add();
-
+ return i;
 }
-
+ return i;
 }
 if (event.keyCode === up_arrow_key) {
 if (get_occupants_cell(protagonist.x, protagonist.y - 1) !==  undefined) {
 add();
-
+ return i;
 }
-
+ return i;
 }
 if (event.keyCode === down_arrow_key) {
 if (get_occupants_cell(protagonist.x, protagonist.y + 1) !==  undefined) {
 add();
-
+ return i;
 }
-
+ return i;
 }
-
+ return i;
 }
 
 
